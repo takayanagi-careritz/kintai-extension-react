1789e9f55a4a90668fd1529bb3b311b34b9f074d
takayanagi-careritz <yoichi.takayanagi@careritztc.co.jp>
2025-07-26 15:42:47 +0900

    update: 背景スクリプトの追加とビルド設定の更新
    Adds background script and updates build configuration
    
    Introduces a background script to handle action clicks, enhancing functionality.
    
    Updates build settings to incorporate the new script and includes a plugin for copying necessary files to the distribution directory, ensuring all assets are properly packaged.
    
    Improves project structure and prepares for future enhancements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,51 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
   },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/main.tsx"),
+        background: resolve(__dirname, "src/background.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+
+  plugins: [
+    react(),
+    copy({
+      verbose: true,
+      hook: "writeBundle",
+      targets: [
+        // publicファイル(アイコンなど) を dist/public へコピー
+        {
+          src: "public/*",
+          dest: "dist/public",
+        },
+        // manifest.json を distへコピー
+        {
+          src: "manifest.json",
+          dest: "dist",
+        },
+        // index.htmlなどchrome extensionで使用するhtmlファイルをdistへコピー
+        {
+          src: "*.html",
+          dest: "dist",
+        },
+      ],
+    }),
+  ],
 });
